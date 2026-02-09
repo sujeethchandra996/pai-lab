3a1afd0df6e0a2af7770dd642af262f40ccdcde1
sujeethchandra996 <gavvalasujeeth@gmail.com>
2026-02-09 13:57:17 +0530

    Add files via upload

---
+++ waterjugbfs.py
@@ -0,0 +1,84 @@
+from collections import deque
+
+def Solution(a, b, target):
+    m = {}
+    isSolvable = False                                                                              
+    path = []
+    q = deque()
+
+    q.append((0, 0))
+
+    while len(q) > 0:
+        u = q.popleft()
+
+        if (u[0], u[1]) in m:
+            continue
+
+        if u[0] > a or u[1] > b or u[0] < 0 or u[1] < 0:
+            continue
+
+        path.append([u[0], u[1]])
+        m[(u[0], u[1])] = 1
+
+        if u[0] == target or u[1] == target:
+            isSolvable = True
+
+            if u[0] == target:
+                if u[1] != 0:
+                    path.append([u[0], 0])
+            else:
+                if u[0] != 0:
+                    path.append([0, u[1]])
+
+            for state in path:
+                print("(", state[0], ",", state[1], ")")
+
+            break
+
+       
+        q.append((u[0], b))
+        q.append((a, u[1]))
+
+      
+        q.append((0, u[1]))
+        q.append((u[0], 0))
+
+       
+        for ap in range(max(a, b) + 1):
+            c = u[0] + ap
+            d = u[1] - ap
+            if c == a or (d == 0 and d >= 0):
+                q.append((c, d))
+
+            c = u[0] - ap
+            d = u[1] + ap
+            if (c == 0 and c >= 0) or d == b:
+                q.append((c, d))
+
+    if not isSolvable:
+        print("Solution not possible")
+
+
+if __name__ == "__main__":
+    Jug1 = int(input("Enter the capacity of Jug1: "))
+    Jug2 = int(input("Enter the capacity of Jug2: "))
+    target = int(input("Enter the target: "))
+
+    print("Path from initial state to solution state ::")
+    Solution(Jug1, Jug2, target)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
